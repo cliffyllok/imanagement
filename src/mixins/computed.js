d1b12586765d431aa8849f483c66d32254274252
cliffy <cliffyllok@gmail.com>
2018-11-18 13:45:22 +0800

    Initial commit

---
+++ src/mixins/computed.js
@@ -0,0 +1,20 @@
+const computed = {
+  computed: {
+    pages() {
+      if (
+        this.pagination.rowsPerPage == null ||
+        this.pagination.totalItems == null
+      )
+        return 0;
+
+      return Math.ceil(
+        this.pagination.totalItems / this.pagination.rowsPerPage
+      );
+    },
+    printable() {
+      return this.$store.state.hideNav;
+    }
+  }
+};
+
+export default computed;
